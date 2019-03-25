31648fc6d329517b750daf01438fc6852cba5581
Tom Marks <thomas.o.marks@gmail.com>
2019-03-25 16:15:05 -0400

    feat(scaffold): Added mininal project files

---
+++ setup.py
@@ -0,0 +1,16 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="config-composer",
+    version="0.0.1",
+    packages=find_packages(),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    setup_requires=[],
+    install_requires=[],
+    tests_require=[],
+)
