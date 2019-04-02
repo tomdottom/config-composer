989da01f343708b525b0564f85b927be927e7c95
Tom Marks <thomas.o.marks@gmail.com>
2019-04-02 15:25:56 -0400

    feat(mvp): Added Sources to select first available source

--- config_composer/core.py
+++ config_composer/core.py
@@ -1,3 +1,14 @@
+class Sources:
+    def __init__(self, *args):
+        self._sources = args
+
+    def __get__(self, obj, objtype):
+        return next(filter(None, (
+            source.__get__(obj, objtype)
+            for source in self._sources
+        )))
+
+
 class ModelMeta(type):
     pass
 
--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -21,3 +21,52 @@ def test_aws_paramater(aws_parameter_fixtures, random_string):
         foo = AWS.Parameter(path="/foo/bar/baz")
 
     assert MyConfig.foo == random_string
+
+
+def test_selectes_first_good_source(aws_parameter_fixtures, random_string):
+    os.environ["FOO_BAR_BAZ"] = "This is the string you are looking for"
+
+    class MyConfig(Config):
+        foo = Sources(
+            Env(path="FOO_BAR_BAZ"),
+            AWS.Parameter(path="/foo/bar/baz"),
+        )
+
+    assert MyConfig.foo == os.environ["FOO_BAR_BAZ"]
+
+
+def test_fall_through_source_options(aws_parameter_fixtures):
+    del os.environ["FOO_BAR_BAZ"]
+    random_string = aws_parameter_fixtures
+
+    class MyConfig(Config):
+        foo = Sources(
+            Env(path="FOO_BAR_BAZ"),
+            AWS.Parameter(path="/foo/bar/baz"),
+        )
+
+    assert MyConfig.foo == random_string
+
+
+def test_inheritance(aws_parameter_fixtures):
+    os.environ["FOO_BAR_BAZ"] = "WAT"
+    random_string = aws_parameter_fixtures
+
+    class MyConfigDev(Config):
+        foo = Sources(
+            Env(path="FOO_BAR_BAZ"),
+        )
+        bar = Sources(
+            Env(path="FOO_BAR_BAZ"),
+        )
+
+    class MyConfig(MyConfigDev):
+        foo = Sources(
+            AWS.Parameter(path="/foo/bar/baz"),
+        )
+
+    assert MyConfigDev.foo == "WAT"
+    assert MyConfigDev.bar == "WAT"
+
+    assert MyConfig.foo == random_string
+    assert MyConfig.bar == "WAT"
